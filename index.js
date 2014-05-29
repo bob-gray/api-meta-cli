6c4d43b1aab29d0404df7045fa34e562d5ab0d60
Bob Gray <gray.bob98@gmail.com>
2014-05-29 13:14:38 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+
+"use strict";
+
+var resolve = require("resolve"),
+	fs = require("fs"),
+	path = require("path"),
+	workingDir = process.cwd(),
+	localApiMeta;
+
+try {
+	localApiMeta = requireLocal("api-meta/src/cli");
+
+} catch (error) {
+	logError();
+}
+
+localApiMeta.run();
+
+function requireLocal (id) {
+	var localPath = resolve.sync(id, {
+		basedir: workingDir
+	});
+
+	return require(localPath);
+}
+
+function logError () {
+	var message = getErrorMessage();
+
+	console.error(message, workingDir);
+}
+
+function getErrorMessage () {
+	var messagePath = path.join(__dirname, "error-message.txt"),
+		message = fs.readFileSync(messagePath, "utf-8");
+
+	return message;
+}
